cd9902a678df2f8363b3f8ca74939c1dd1280e8f
decoletreinamento <decoletreinamento@gmail.com>
2026-09-18 14:32:19 -0300

    ajustess

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'decole-portal-v1';
+const assetsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './fundo.png'
+];
+
+// Instalação do Service Worker e gravação em cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+// Ativação do Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Interceção de pedidos de rede para permitir funcionamento offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
